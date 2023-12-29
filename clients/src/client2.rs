19b6b16adcbc62eea43a9e825fb1b5602b98f5e5
Quin-Darcy <quintondarcy@csus.edu>
2023-12-28 20:47:34 -0800

    removed uneeded comments

--- clients/src/client2.rs
+++ clients/src/client2.rs
@@ -11,37 +11,6 @@ use byteorder::{ByteOrder, BigEndian};
 use aes_crypt;
 
 
-/* AES_CRYPT API
-
-pub fn gen_key() -> [u8; 4 * Nk as usize] {
-    rand::thread_rng().gen::<[u8; 4 * Nk as usize]>()
-}
-
-pub fn encrypt(plain_text: &[u8], key: &[u8]) -> Vec<u8> {
-    let key_schedule: Vec<u32> = key_expansion(key);
-    let mut data: Data = Data::from_plain_text_bytes(plain_text);
-
-    for i in 0..data.states.len() {
-        cipher(&mut data.states[i], &key_schedule);
-    }
-
-    data.to_encrypted_bytes()
-}
-
-pub fn decrypt(cipher_text: &[u8], key: &[u8]) -> Vec<u8> {
-    let key_schedule: Vec<u32> = key_expansion(key);
-    let mut data: Data = Data::from_cipher_text_bytes(cipher_text);
-
-    for i in 0..data.states.len() {
-        inv_cipher(&mut data.states[i], &key_schedule);
-    }
-
-    data.to_decrypted_bytes()
-}
-
-*/
-
-
 pub struct Client2 {
     key: Vec<u8>,
 }
